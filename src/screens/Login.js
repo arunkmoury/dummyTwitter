7cd39d522571ddc7f3f0d9d250b371026b61f921
Arun Kumar <arunkmoury@gmail.com>
2018-02-21 18:13:21 +0530

    Login page and sign up added with style

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { 
+    View, 
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+    } from 'react-native';
 
-const Login = () => {
-    return (
-        <View>
-            <Text>Login</Text>
-        </View>
-    );
+class Login extends Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                
+                {
+                    this.props.type==="Sign Up"?<TextInput 
+                    style={styles.inputBox} 
+                    underlineColorAndroid='rgba(0,0,0,0)' 
+                    placeholder="Name"
+                    placeholderTextColor = "#ffffff"
+                    selectionColor="#fff"
+                    onSubmitEditing={()=> this.email.focus()}
+                />:null
+                }
+                
+                <TextInput 
+                    style={styles.inputBox} 
+                    underlineColorAndroid='rgba(0,0,0,0)' 
+                    placeholder="Email"
+                    placeholderTextColor = "#ffffff"
+                    selectionColor="#fff"
+                    keyboardType="email-address"
+                    onSubmitEditing={()=> this.password.focus()}
+                    ref={(input) => this.email = input}
+                />
+                <TextInput 
+                    style={styles.inputBox} 
+                    underlineColorAndroid='rgba(0,0,0,0)' 
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    placeholderTextColor = "#ffffff"
+                    ref={(input) => this.password = input}
+                />
+                <TouchableOpacity style={styles.button} onPress={this.props.buttonPressed} >
+                    <Text style={styles.buttonText}>{this.props.type}</Text>
+                </TouchableOpacity>  
+            </View>
+        );
+    }
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+    container : {
+      flexGrow: 1,
+      justifyContent:'center',
+      alignItems: 'center'
+    },
+  
+    inputBox: {
+      width:300,
+      backgroundColor:'#9575cd',
+      borderRadius: 20,
+      paddingHorizontal:16,
+      fontSize:16,
+      color:'#ffffff',
+      marginVertical: 10
+    },
+    button: {
+    width:300,
+      backgroundColor:'#ff3d00',
+       borderRadius: 20,
+    marginVertical: 10,
+        paddingVertical: 13
+    },
+    buttonText: {
+      fontSize:16,
+      fontWeight:'500',
+      color:'#ffffff',
+      textAlign:'center'
+    }
+    
+  });
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { Grid, Row, Container } from 'native-base';
+import { View, Text, StyleSheet, Button, TouchableOpacity, Modal } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import DrawerNav from '../screens/Profile';
+import Login from '../screens/Login';
 
 export class Welcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {modalVisible: false};
+    }
     render(){
         return (
-            <Container>
-                    <Grid>
-                        <Row size={3}>
-                            <Text style={styles.welcomeStyle}>Welcome to Dummy Twitter</Text>
-                        </Row>
-                        <Row size={2}>
-                            <Button title="Login" style={{width: 100}} onPress={() => this.props.navigation.navigate('Home')} />
-                        </Row>
-                    </Grid>
-            </Container>
+            <View style={styles.container}>
+                <Login type="Login" buttonPressed={() => this.props.navigation.navigate('Home')}/>
+                <View style={styles.signupStyle}>
+                    <TouchableOpacity 
+                        style={styles.button} 
+                        onPress={() => {this.setState({modalVisible: true})}} 
+                    >
+                        <Text style={styles.buttonText}>Sign Up</Text>
+                    </TouchableOpacity> 
+                </View>
+                <Modal 
+                    visible={this.state.modalVisible}
+                    animationType={'fade'}
+                    onRequestClose={() => this.setState({modalVisible: false})}
+                    position='center'
+                >
+                    <View style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        alignContent: 'center',
+                        height: 500
+                    }}>
+                        <Login type="Sign Up" />
+                    
+                        <Button
+                            onPress={()=>this.setState({modalVisible: false})}
+                            title="Close Modal"
+                        />
+                    </View>
+                </Modal>
+            </View>
         );
     }
 }
@@ -36,12 +61,30 @@ const StackNav = StackNavigator({
 export default StackNav;
 
 const styles = StyleSheet.create({
-    welcomeStyle: {
-        justifyContent: 'center',
-        alignSelf: 'center',
+    container : {
+        backgroundColor:'#4fc3f7',
+        flex: 1,
+        alignItems:'center',
+        justifyContent :'center'
     },
-    loginStyle: {
-        justifyContent: 'center',
-        alignSelf: 'center',
+    signupStyle: {
+        flexGrow: 1,
+        alignItems:'flex-start',
+        justifyContent :'center',
+        paddingVertical:20,
+        flexDirection:'row'
+    },
+    button: {
+        width:300,
+        backgroundColor:'#ff3d00',
+        borderRadius: 20,
+        marginVertical: 10,
+        paddingVertical: 13
+    },
+    buttonText: {
+        fontSize:16,
+        fontWeight:'500',
+        color:'#ffffff',
+        textAlign:'center'
     }
 });
